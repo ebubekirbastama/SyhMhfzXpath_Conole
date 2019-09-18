b2557368a4a6b08723b4762b9f49dd02f5bba313
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2019-09-19 01:21:17 +0300

    Add files via upload

---
+++ Xpath Conole.js
@@ -0,0 +1,22 @@
+//Siyah Muhafiz Web Browser Console XPath Kullanımı\\
+//-------------------------------------------------\\
+                   //Ham Hali Kullanımı\\
+//--------------------------------------------------\\
+//Örnek:
+//-------------------------------------------------------
+//$x("")
+
+//Örnek:
+//-------------------------------------------------------
+//$x("//div[@class='title']//a")[0].getAttribute('href');
+
+
+var syhmhz = new Array();
+for (var i = 0; i < $x("//div[@class='title']//a").length; i++) 
+{
+  syhmhz.push($x("//div[@class='title']//a")[i].getAttribute('href'));
+}
+ for (var j = 0; j < syhmhz.length; j++) 
+{
+  document.write(syhmhz[j]+"\r");
+}
